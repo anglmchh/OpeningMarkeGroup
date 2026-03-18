403dfc8abf5f29bfbb4635bc30132a335c567ac5
Marvin <marvinesport@gmail.com>
2026-03-18 14:00:41 -0400

    Revert "update"
    This reverts commit b5f2c7166aee4c40b13005e5c5659f9b713ece50.

--- seped_connector/models/__init__.py
+++ seped_connector/models/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from . import seped_config
-from . import sale_order_seped
-from . import seped_extensions
+# from . import sale_order_seped
+# from . import seped_extensions
